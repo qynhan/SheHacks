790bf77e5bf8e9d17fcc73da6177f6626b952bb6
qynhan <qhan33@uwo.ca>
2023-01-15 04:20:54 -0500

    completed version

--- frontend_app/src/App.js
+++ frontend_app/src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React, {useState} from 'react' 
 import axios from 'axios'
 import Button from 'react-bootstrap/Button';
-import Badge from 'react-bootstrap/Badge';
-// import 'bootstrap/dist/css/bootstrap.min.css';
+import Form from 'react-bootstrap/Form';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import './App.css';
 
 function App() {
   const [data,setData]=useState(null);
@@ -28,9 +31,22 @@ function App() {
   
   return (
     <div className="App" style={{backgroundColor: '#917ef3', fontFamily: 'PlayfairWght'}}> 
-      <h1>Free Your Eyes!</h1>
-      <h3>Input text you want to summarize below:</h3>
-      <input type="text" onChange={getData}/>
+      <div class="row">
+        <div class="col-sm">
+          <h1>Free Your Eyes!</h1>
+          <h4>Harness the power of tool to improve reading efficiency. </h4>
+        </div>
+        <div class="col-sm">
+          <p>Don’t want to read all Terms&Conditions before consent?</p>
+          <p>Having dyslexia? Too lazy for reading long paragraph?</p>
+          <p>No worries!</p>
+          <p>This AI is here to free your eyes! Join the “Free Your Eyes” by SheHacks to generate, categorize and organize text, enjoy your experience.</p>
+        </div>
+      </div>
+      <Form>
+        <input type="text" placeholder="Input text you wish to summarize here" onChange={getData}/>
+      </Form>
+      
       {
         print?
         <p>{data}</p>
@@ -43,9 +59,8 @@ function App() {
         setPrint(true)
         summarize()
       }}>Print Value</Button>{' '}
-
-      <p>Harness the power of tool to improve reading efficiency. Don’t want to read all Terms&Conditions before consent? Having dyslexia? Too lazy for reading long paragraph? No worries! This AI is here to free your eyes! Join the “Free Your Eyes” by SheHacks to generate, categorize and organize text, enjoy your experience.</p>
-
+      
+        
     </div>
   );
 }
