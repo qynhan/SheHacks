fd515e4ecc70fb2c1c96356ee0c917eb13738833
qynhan <qhan33@uwo.ca>
2023-01-14 23:51:23 -0500

    connected to flask server

--- frontend_app/src/App.js
+++ frontend_app/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import React, {useState} from 'react' 
+import axios from 'axios'
 
 function App() {
   const [data,setData]=useState(null);
@@ -9,11 +10,18 @@ function App() {
   {
     setData(val.target.value)
     setPrint(false)
-    console.warn(val.target.value)
+    // console.warn(val.target.value)
+  }
+
+  async function summarize(){
+    let response = await axios.post("/summarize", {
+      text: data
+    })
+    console.log(response.data)
   }
   
   return (
-    <div className="App">
+    <div className="App"> 
       <h1>SheHacks Project (edit name later) </h1>
       <h3>Input text you want to summarize below:</h3>
       <input type="text" onChange={getData}/>
@@ -22,7 +30,10 @@ function App() {
         <p>{data}</p>
         :null
       }
-      <button onClick={()=>setPrint(true)}>Print Value</button>
+      <button onClick={()=>{
+        setPrint(true)
+        summarize()
+      }}>Print Value</button>
     </div>
   );
 }
